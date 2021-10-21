07e930a68ef1cca1ef49a5b07d9475fec5ad1a6f
Kan1aSorata <FengYuHe.BiliBili@hotmail.com>
2021-10-21 11:21:11 +0800

    feat: 创建项目

---
+++ app/src/main/java/com/fengyuhe/music/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.fengyuhe.music
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
